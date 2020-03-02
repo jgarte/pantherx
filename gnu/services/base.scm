dc2ad9f3a088c3dbc3c4773226ed6d40ace38728
Reza Alizadeh Majd <r.majd@pantherx.org>
2020-03-02 21:30:46 +0330

    pantherx: revert guix authorized keys
    Signed-off-by: Reza Alizadeh Majd <r.majd@pantherx.org>

--- gnu/services/base.scm
+++ gnu/services/base.scm
@@ -1601,8 +1601,7 @@ archive' public keys, with GUIX."
 
 (define %default-authorized-guix-keys
   ;; List of authorized substitute keys.
-  (list (file-append guix "/share/guix/berlin.guixsd.org.pub")
-        (file-append guix "/share/guix/build.pantherx.org.pub")))
+  (list (file-append guix "/share/guix/berlin.guixsd.org.pub")))
 
 (define-record-type* <guix-configuration>
   guix-configuration make-guix-configuration
